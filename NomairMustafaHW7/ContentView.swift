f4b4f69937afa2a2730ba0767a2f25236c8e167e
Mustafa Nomair <nomair@usc.edu>
2025-03-31 22:13:07 -0700

    Initial Commit

---
+++ NomairMustafaHW7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NomairMustafaHW7
+//
+//  Created by Student on 3/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NomairMustafaHW7/NomairMustafaHW7App.swift
@@ -0,0 +1,17 @@
+//
+//  NomairMustafaHW7App.swift
+//  NomairMustafaHW7
+//
+//  Created by Student on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct NomairMustafaHW7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
